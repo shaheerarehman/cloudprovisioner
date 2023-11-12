c7ae430d953d170109fca3a25f4d030f023f9ef8
Shaheer Rehman <shaheerrehman766@gmail.com>
2023-11-12 17:50:24 -0500

    deploy form simple

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
      path('main/', views.main, name="main"),
      path('main2/', views.main2, name="main2"),
+     path('deploy/', views.deploy_ec2_instance, name='deploy_ec2'),
 ]
